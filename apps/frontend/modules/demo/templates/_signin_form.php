3ca77e26062908ecf955a4e03ad93547f0200a33
smallkaa(fazer) <git.nospam@vershinin.ru>
2011-02-06 16:15:22 +0300

    Теперь форма логина работает корректно

--- apps/frontend/modules/demo/templates/_signin_form.php
+++ apps/frontend/modules/demo/templates/_signin_form.php
@@ -6,16 +6,20 @@
                     }); 
             });
         </script>
-    <div class="sign_popup" style="display:none;">
-            <?php if($form['username']->hasError()):?>
-            <p class="error"> We don't seem to know you yet. <a href="<?php echo url_for('/customer/new') ?>">Sign Up</a> or try another username.</p>
-            <?php endif; ?>
+    <div class="sign_popup" <?if(!$form['username']->hasError()){?> style="display:none;"<?}?>>
+            <?php
+            $err = ""; 
+            if($form['username']->hasError()):?>
+            <p class="error"> We don't seem to know you yet.<br /> <a href="<?php echo url_for('/customer/new') ?>">Sign Up</a> or try another username.</p>
+            <?php
+             $err = " error_text"; 
+             endif; ?>
             <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
             <?php echo $form['_csrf_token']; ?>
             <strong>User name</strong>
-            <?php echo $form['username']->render(array('class'=>"text error_text")); ?>
+            <?php echo $form['username']->render(array('class'=>"text".$err)); ?>
             <strong>Password</strong>
-            <?php echo $form['password']->render(array('class'=>"text")); ?>
+            <?php echo $form['password']->render(array('class'=>"text".$err)); ?>
                 <div class="bottom_sign_popup">
                     <input type="submit" value="" class="sign_in" />
                     <div class="memory">
@@ -25,7 +29,7 @@
                 </div>
             </form>
         </div>
-    <div class="sign"><a href="<?php echo url_for('/customer/new') ?>">Sign Up</a>|<a href="#" id="signin_href">Sign In</a> <!-- <div class="fb"><img src="/img/temp/fb.png" /></div> --> </div>
+    <div class="sign"><a href="<?php echo url_for('/customer/new') ?>">Sign Up</a>|<a href="#" id="signin_href">Sign In</a>  <div class="fb"><img src="/img/temp/fb.png" /></div> </div>
 <?php else: ?>
     <div class="sign"><a href="#"><?php echo $sf_user->getUsername();?></a>|<a href="<? echo url_for('@sf_guard_signout'); ?>">Logout</a></div>
 <?php endif; ?>
